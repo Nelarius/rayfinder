4df39fa7a165f10e7582847c999bf02e60a6ec6a
Johann Muszynski <johann.muszynski@gmail.com>
2023-10-29 11:47:00 +0200

    Hello GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,50 @@
+#include <chrono>
 #include <cstdio>
+
+#include <thread>
+
+#include <GLFW/glfw3.h>
+
+inline constexpr int defaultWindowWidth = 640;
+inline constexpr int defaultWindowHeight = 480;
 
 int main()
 {
-    std::printf("Hello, world\n");
+    if (!glfwInit())
+    {
+        std::fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow* const window = glfwCreateWindow(
+        defaultWindowWidth, defaultWindowHeight, "pt-playground 🛝", nullptr, nullptr);
+    if (!window)
+    {
+        std::fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    {
+        glfwMakeContextCurrent(window);
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+
+            if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            {
+                glfwSetWindowShouldClose(window, GLFW_TRUE);
+            }
+
+            glfwSwapBuffers(window);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(16));
+        }
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
