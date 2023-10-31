5a9f1cc5c460d1da71b65081a1a5248cc0655c45
Johann Muszynski <johann.muszynski@gmail.com>
2023-10-31 14:04:44 +0200

    Extent: additional type aliases

--- src/common/extent.hpp
+++ src/common/extent.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace pt
 {
 template<typename T>
@@ -9,7 +11,8 @@ struct Extent2
     T y = T(0);
 };
 
-using Extent2i = Extent2<int>;
+using Extent2i = Extent2<std::int32_t>;
+using Extent2u = Extent2<std::uint32_t>;
 
 template<typename T>
 constexpr bool operator==(const Extent2<T>& lhs, const Extent2<T>& rhs) noexcept
