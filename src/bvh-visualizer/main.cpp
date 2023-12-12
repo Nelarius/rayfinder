c1f10e1e9920fee0150f3d19c62fbdbfbece80d9
Johann Muszynski <johann.muszynski@gmail.com>
2023-12-12 21:00:54 +0200

    bvh-visualizer: output image as png

--- src/bvh-visualizer/main.cpp
+++ src/bvh-visualizer/main.cpp
@@ -6,15 +6,17 @@
 #include <common/ray_intersection.hpp>
 #include <common/units/angle.hpp>
 
+#include <stb_image_write.h>
+
 #include <algorithm>
 #include <cstdint>
-#include <iostream>
+#include <cstdio>
 
 using namespace nlrs;
 
 inline constexpr Extent2i imageSize = Extent2i{1280, 720};
 
-void printHelp() { std::cout << "Usage: bvh-visualizer <input_gltf_file>\n" << std::endl; }
+void printHelp() { std::printf("Usage: bvh-visualizer <input_gltf_file>\n"); }
 
 int main(int argc, char** argv)
 {
@@ -49,16 +51,14 @@ int main(int argc, char** argv)
             aspectRatio(imageSize));
     }();
 
-    // Outputs binary PPM image format, https://netpbm.sourceforge.net/doc/ppm.html
-    std::cout << "P6\n";
-    std::cout << imageSize.x << ' ' << imageSize.y << '\n';
-    std::cout << "255\n";
+    std::vector<std::uint32_t> pixelData;
+    pixelData.reserve(imageSize.x * imageSize.y);
+
     for (int i = 0; i < imageSize.y; ++i)
     {
         for (int j = 0; j < imageSize.x; ++j)
         {
             const float u = static_cast<float>(j) / static_cast<float>(imageSize.x);
-            // Binary PPM is stored from top to bottom. Invert v to flip the image vertically.
             const float v = 1.0f - static_cast<float>(i + 1) / static_cast<float>(imageSize.y);
 
             const Ray ray = generateCameraRay(camera, u, v);
@@ -67,12 +67,19 @@ int main(int argc, char** argv)
             BvhStats     bvhStats;
             rayIntersectBvh(ray, bvh.nodes, triangles, FLT_MAX, intersect, &bvhStats);
 
-            const float x = 0.01f * static_cast<float>(bvhStats.nodesVisited);
-            const auto  p = static_cast<std::uint8_t>(std::min(x, 1.0f) * 255.0f);
-
-            std::cout << p << p << p;
+            const float         x = 0.01f * static_cast<float>(bvhStats.nodesVisited);
+            const auto          p = static_cast<std::uint32_t>(std::min(x, 1.0f) * 255.0f);
+            const std::uint32_t pixel = (255u << 24) | (p << 16) | (p << 8) | p;
+            pixelData.push_back(pixel);
         }
     }
+
+    const int numChannels = 4;
+    const int strideBytes = imageSize.x * numChannels;
+
+    [[maybe_unused]] const int result = stbi_write_png(
+        "bvh-visualizer.png", imageSize.x, imageSize.y, numChannels, pixelData.data(), strideBytes);
+    assert(result != 0);
 
     return 0;
 }
