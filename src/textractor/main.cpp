fdab5d1cc35a6a6aa7e3e3006b1e9588a908477b
Johann Muszynski <johann.muszynski@gmail.com>
2024-03-31 19:38:48 +0300

    Fix textractor pixel format

--- src/textractor/main.cpp
+++ src/textractor/main.cpp
@@ -5,7 +5,11 @@
 #include <fmt/core.h>
 #include <stb_image_write.h>
 
+#include <algorithm>
 #include <cstdio>
+#include <cstdint>
+#include <iterator>
+#include <vector>
 
 void printHelp() { std::printf("Usage: textractor <input_gltf_file>\n"); }
 
@@ -23,11 +27,27 @@ int main(int argc, char** argv)
     {
         const auto& texture = model.baseColorTextures[textureIdx];
         const auto  dimensions = texture.dimensions();
-        const auto  pixels = texture.pixels();
+        const auto  pixelsBgra = texture.pixels();
 
         const std::string filename = fmt::format("base_color_texture_{}.png", textureIdx);
         const int         numChannels = 4;
         const int         strideBytes = dimensions.width * numChannels;
+
+        static_assert(sizeof(nlrs::Texture::BgraPixel) == sizeof(std::uint32_t));
+
+        std::vector<std::uint32_t> pixelsRgba;
+        pixelsRgba.reserve(dimensions.width * dimensions.height);
+        std::transform(
+            pixelsBgra.begin(),
+            pixelsBgra.end(),
+            std::back_inserter(pixelsRgba),
+            [](const auto& bgra) {
+                const std::uint32_t b = bgra & 0xffu;
+                const std::uint32_t g = (bgra >> 8) & 0xffu;
+                const std::uint32_t r = (bgra >> 16) & 0xffu;
+                const std::uint32_t a = (bgra >> 24) & 0xffu;
+                return r | (g << 8) | (b << 16) | (a << 24);
+            });
 
         NLRS_ASSERT(
             stbi_write_png(
@@ -35,7 +55,7 @@ int main(int argc, char** argv)
                 dimensions.width,
                 dimensions.height,
                 numChannels,
-                pixels.data(),
+                pixelsRgba.data(),
                 strideBytes) != 0);
     }
 
