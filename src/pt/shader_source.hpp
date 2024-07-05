329d7e047f4140bd4f37c0af5b8ea83d97959c15
Johann Muszynski <johann.muszynski@gmail.com>
2024-07-05 14:54:59 +0300

    Revert "wip bilinear attempt"
    This reverts commit c8d86f74830c5561832b0c0a7be614921b940631.

--- src/pt/shader_source.hpp
+++ src/pt/shader_source.hpp
@@ -1335,92 +1335,32 @@ struct Uniforms {
 fn fsMain(in: VertexOutput) -> @location(0) vec4f {
     let uv = in.texCoord;
     let textureIdx = vec2u(floor(uv * uniforms.framebufferSize));
-    let sampleBufferIdx = textureIdxToLinearIdx(textureIdx);
-    let currentColor = bilinearSample(uv);
+    let sampleBufferIdx = textureIdx.y * u32(uniforms.framebufferSize.x) + textureIdx.x;
+    let sample: array<f32, 3> = sampleBuffer[sampleBufferIdx];
+    let currentColor = vec3f(sample[0], sample[1], sample[2]);
     var color = vec3f(0f);
     if uniforms.frameCount == 0u {
+        accumulationBuffer[sampleBufferIdx] = sample;
         color = currentColor;
-        accumulationBuffer[sampleBufferIdx] = array<f32, 3>(color.r, color.g, color.b);
     } else {
-        let uvVelocity = bilinearVelocity(uv);
+        let uvVelocity = textureLoad(gbufferVelocity, textureIdx, 0).rg;
         let previousUv = uv + uvVelocity;
         if previousUv.x >= 0f && previousUv.x < 1f && previousUv.y >= 0f && previousUv.y < 1f {
-            let previousColor = bilinearAccumulationSample(previousUv);
+            let previousTextureIdx = vec2u(floor(previousUv * uniforms.framebufferSize));
+            let previousSampleBufferIdx = previousTextureIdx.y * u32(uniforms.framebufferSize.x) + previousTextureIdx.x;
+            let previousSample = accumulationBuffer[previousSampleBufferIdx];
+            let previousColor = vec3f(previousSample[0], previousSample[1], previousSample[2]);
             color = 0.1 * currentColor + 0.9 * previousColor;
             accumulationBuffer[sampleBufferIdx] = array<f32, 3>(color.r, color.g, color.b);
         } else {
+            accumulationBuffer[sampleBufferIdx] = sample;
             color = currentColor;
-            accumulationBuffer[sampleBufferIdx] = array<f32, 3>(color.r, color.g, color.b);
         }
     }
 
     let rgb = acesFilmic(uniforms.exposure * color);
     let srgb = pow(rgb, vec3(1f / 2.2f));
     return vec4(srgb, 1f);
-}
-
-fn bilinearSample(uv: vec2f) -> vec3f {
-    let textureIdx = vec2u(floor(uv * uniforms.framebufferSize));
-    let stride = u32(uniforms.framebufferSize.x);
-    let tlIdx = textureIdxToLinearIdx(textureIdx);
-    let trIdx = textureIdxToLinearIdx(textureIdx + vec2u(1u, 0u));
-    let blIdx = textureIdxToLinearIdx(textureIdx + vec2u(0u, 1u));
-    let brIdx = textureIdxToLinearIdx(textureIdx + vec2u(1u, 1u));
-    let tl: array<f32, 3> = sampleBuffer[tlIdx];
-    let tr: array<f32, 3> = sampleBuffer[trIdx];
-    let bl: array<f32, 3> = sampleBuffer[blIdx];
-    let br: array<f32, 3> = sampleBuffer[brIdx];
-    let ctl = vec3f(tl[0], tl[1], tl[2]);
-    let ctr = vec3f(tr[0], tr[1], tr[2]);
-    let cbl = vec3f(bl[0], bl[1], bl[2]);
-    let cbr = vec3f(br[0], br[1], br[2]);
-    let tx = fract(uv.x * uniforms.framebufferSize.x);
-    let top = mix(ctl, ctr, tx);
-    let bottom = mix(cbl, cbr, tx);
-    let ty = fract(uv.y * uniforms.framebufferSize.y);
-    return mix(top, bottom, ty);
-}
-
-fn bilinearAccumulationSample(uv: vec2f) -> vec3f {
-    let textureIdx = vec2u(floor(uv * uniforms.framebufferSize));
-    let stride = u32(uniforms.framebufferSize.x);
-    let tlIdx = textureIdxToLinearIdx(textureIdx);
-    let trIdx = textureIdxToLinearIdx(textureIdx + vec2u(1u, 0u));
-    let blIdx = textureIdxToLinearIdx(textureIdx + vec2u(0u, 1u));
-    let brIdx = textureIdxToLinearIdx(textureIdx + vec2u(1u, 1u));
-    let tl: array<f32, 3> = accumulationBuffer[tlIdx];
-    let tr: array<f32, 3> = accumulationBuffer[trIdx];
-    let bl: array<f32, 3> = accumulationBuffer[blIdx];
-    let br: array<f32, 3> = accumulationBuffer[brIdx];
-    let ctl = vec3f(tl[0], tl[1], tl[2]);
-    let ctr = vec3f(tr[0], tr[1], tr[2]);
-    let cbl = vec3f(bl[0], bl[1], bl[2]);
-    let cbr = vec3f(br[0], br[1], br[2]);
-    let tx = fract(uv.x * uniforms.framebufferSize.x);
-    let top = mix(ctl, ctr, tx);
-    let bottom = mix(cbl, cbr, tx);
-    let ty = fract(uv.y * uniforms.framebufferSize.y);
-    return mix(top, bottom, ty);
-}
-
-@must_use
-fn bilinearVelocity(uv: vec2f) -> vec2f {
-    let textureIdx = vec2u(floor(uv * uniforms.framebufferSize));
-    let tl = textureLoad(gbufferVelocity, textureIdx, 0).rg;
-    let tr = textureLoad(gbufferVelocity, textureIdx + vec2u(1u, 0u), 0).rg;
-    let bl = textureLoad(gbufferVelocity, textureIdx + vec2u(0u, 1u), 0).rg;
-    let br = textureLoad(gbufferVelocity, textureIdx + vec2u(1u, 1u), 0).rg;
-    let tx = fract(uv.x * uniforms.framebufferSize.x);
-    let top = mix(tl, tr, tx);
-    let bottom = mix(bl, br, tx);
-    let ty = fract(uv.y * uniforms.framebufferSize.y);
-    return mix(top, bottom, ty);
-}
-
-@must_use
-fn textureIdxToLinearIdx(idx: vec2u) -> u32 {
-    let clampedIdx = min(idx, vec2u(uniforms.framebufferSize) - vec2u(1));
-    return clampedIdx.y * u32(uniforms.framebufferSize.x) + clampedIdx.x;
 }
 
 @must_use
